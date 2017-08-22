cc0cb6664989b22b3b6870f50e38dd52f34e078e
Luo Rock <luorock@LuodeMacBook-Air.local>
2017-08-21 22:16:49 -0700

    Initial Commit

---
+++ Foodie Me/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Foodie Me
+//
+//  Created by Luo Rock on 2017-08-21.
+//  Copyright © 2017 Luo Rock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
