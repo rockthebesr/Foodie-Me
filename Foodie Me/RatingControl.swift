3311501abe8ec63ed5d0277d379b6cf7104105bd
Luo Rock <luorock@LuodeMacBook-Air.local>
2017-08-24 00:46:29 -0700

    inital commit

---
+++ Foodie Me/RatingControl.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControl.swift
+//  Foodie Me
+//
+//  Created by Luo Rock on 2017-08-23.
+//  Copyright © 2017 Luo Rock. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
